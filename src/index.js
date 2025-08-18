0e607597c1c353ef208e90d9fb9ca017b69e6c36
Sureshkumar <sureshkumar@navadhiti.com>
2025-08-18 12:07:34 +0530

    remove the csp.js file and remove the imported csp in the index.js file

--- src/index.js
+++ src/index.js
@@ -1,34 +1,21 @@
 import React from "react";
-import { render } from "react-dom";
+import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import App from "./App";
 import "./assets/styles/index.scss";
 import store from "./store/configureStore";
 import "./index.css";
 import { BrowserRouter as Router } from "react-router-dom";
-import { getCSP } from "./csp";
 
-const injectCSP = () => {
-  try {
-    const cspContent = getCSP(process.env); // Pass environment variables
-    const metaTag = document.createElement("meta");
-    metaTag.httpEquiv = "Content-Security-Policy";
-    metaTag.content = cspContent.trim();
-    document.head.appendChild(metaTag);
-  } catch (error) {
-    console.error("Failed to inject CSP:", error);
-  }
-};
+const container = document.getElementById("root");
+const root = createRoot(container);
 
-injectCSP();
-
-render(
+root.render(
   <React.StrictMode>
     <Router>
       <Provider store={store}>
         <App />
       </Provider>
     </Router>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
