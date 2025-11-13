ae289b04c7a03a67d02af49067967670fc2e895c
Kathir2315 <kathirmskgt@gmail.com>
2025-11-13 12:50:34 +0530

    changed the awss3 config file

--- src/config/awsS3.js
+++ src/config/awsS3.js
@@ -1,4 +1,4 @@
-// import { S3Client } from "@aws-sdk/client-s3";
+import { S3Client } from "@aws-sdk/client-s3";
 
 // export default new S3Client({
 //   region: process.env.REACT_APP_AWS_S3_REGION,
@@ -8,10 +8,21 @@
 //   },
 // });
 
-import { S3Client } from "@aws-sdk/client-s3";
+//import { S3Client } from "@aws-sdk/client-s3";
 
-const s3Client = new S3Client({
-  region: process.env.REACT_APP_AWS_S3_REGION || "us-east-1", // dummy region for E2E
+//const s3Client = new S3Client({
+//region: process.env.REACT_APP_AWS_S3_REGION || "us-east-1", // dummy region for E2E
+//endpoint: process.env.REACT_APP_S3_ENDPOINT,
+//forcePathStyle: true,
+//credentials: {
+// accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+// secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+// },
+//});
+//export default s3Client;
+
+export default new S3Client({
+  region: "us-east-1", // dummy region for E2E
   endpoint: process.env.REACT_APP_S3_ENDPOINT,
   forcePathStyle: true,
   credentials: {
@@ -19,4 +30,3 @@ const s3Client = new S3Client({
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
   },
 });
-export default s3Client;
