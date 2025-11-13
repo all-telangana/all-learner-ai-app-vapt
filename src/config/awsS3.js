5eebd02f24db5221b6834915f4f937bc93633c0d
Sureshkumar <sureshkumar@navadhiti.com>
2025-11-13 11:58:22 +0530

    change the aws config file and .env file

--- src/config/awsS3.js
+++ src/config/awsS3.js
@@ -1,9 +1,22 @@
+// import { S3Client } from "@aws-sdk/client-s3";
+
+// export default new S3Client({
+//   region: process.env.REACT_APP_AWS_S3_REGION,
+//   credentials: {
+//     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
+//     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
+//   },
+// });
+
 import { S3Client } from "@aws-sdk/client-s3";
 
-export default new S3Client({
-  region: process.env.REACT_APP_AWS_S3_REGION,
+const s3Client = new S3Client({
+  region: process.env.REACT_APP_AWS_S3_REGION || "us-east-1", // dummy region for E2E
+  endpoint: process.env.REACT_APP_S3_ENDPOINT,
+  forcePathStyle: true,
   credentials: {
     accessKeyId: process.env.REACT_APP_AWS_ACCESS_KEY_ID,
     secretAccessKey: process.env.REACT_APP_AWS_SECRET_ACCESS_KEY,
   },
 });
+export default s3Client;
